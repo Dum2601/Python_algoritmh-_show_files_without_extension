b11ea309560eab8a91b3918122574ce38ade18db
Douglas Barbosa <Doug_b_souza@hotmail.com>
2025-05-01 14:31:22 -0300

    First Commit

---
+++ extract_name_from_file/extract_name_file.py
@@ -0,0 +1,4 @@
+from utils.file_operations import Files
+
+files = Files()
+print(files.format_files())
---
+++ extract_name_from_file/utils/file_operations.py
@@ -0,0 +1,60 @@
+import os
+
+class Files:
+    def __init__(self):
+
+        self.directory_path = r'C:\Users\Dum\Documents\Programação\Praticando\Python\Exercícios criados por IA baseado na DIO\extract_name\extract_name_from_file\utils\files_to_be_searched'
+
+    def list_files(self):
+
+        if os.path.exists(self.directory_path):
+
+            self.inside_dir = os.listdir(self.directory_path)
+            return self.inside_dir
+        
+        else:
+
+            return 'O diretório não foi encontrado'
+
+    def format_files(self):
+
+        def show_files():
+
+            self.files_in_dir = []            
+            self.files_name = self.list_files()
+
+            for name in self.files_name:
+
+                self.files_in_dir.append(name)
+            
+            return self.files_in_dir
+        
+        def hide_extension_file():
+           
+            raw_names = show_files()
+            formated = []
+
+            for name in raw_names:
+                name_without_ext = os.path.splitext(name)[0] 
+                formated.append(name_without_ext)
+
+            return formated
+
+        try:
+
+            return hide_extension_file()  
+        
+        except Exception as err:
+
+            print('Erro encontrado na função de formatação.\n',
+                f'Erro informado: {err}')
+            
+    @property
+    def show_files(self):
+        self.format_files = self.format_files()
+        return self.format_files
+
+
+
+
+
